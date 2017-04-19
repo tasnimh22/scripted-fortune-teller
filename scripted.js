03128b77e01e4a90d964eb864988674e910b8668
tasnimh22 <tasnimh4@gmail.com>
2017-04-19 21:57:53 +0000

    milestone 4 completed

--- scripted.js
+++ scripted.js
@@ -1,7 +1,10 @@
 $(document).ready(function(){
    
   var array = ["most likely", "try again", "unlikely", "probably"];
-   
+   $("button").click(function(){
+      var number= Math.floor(Math.random() * array.length); 
+      $("#fortune").html(array[number])
+   });
    
    
    
